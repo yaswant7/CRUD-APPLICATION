8f0c007c2eaaea8ee90256758184dbc2b981c1cb
yaswant7 <ykamineni@gmail.com>
2022-12-22 16:29:42 +0530

    Crud Application Code

---
+++ DAY7 CRUD/CRUD.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CRUD.aspx.cs" Inherits="YASHTRY.DAY7_CRUD.CRUD" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 33px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <table class="auto-style1">
+                <tr>
+                    <td colspan="2">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:Label ID="Label4" runat="server" Text="CRUD APPLICATION" BackColor="#CCD5AE" Font-Bold="True" ForeColor="#005F73"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label5" runat="server" Text="ID" BackColor="#CCD5AE" Font-Bold="True" ForeColor="#005F73"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                        <asp:Button ID="Button5" runat="server" Text="Select" BackColor="#A1CCA5" OnClick="Button5_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Name" BackColor="#CCD5AE" Font-Bold="True" ForeColor="#005F73"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Label ID="Label2" runat="server" Text="Age" BackColor="#CCD5AE" Font-Bold="True" ForeColor="#005F73"></asp:Label>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Label ID="Label3" runat="server" Text="City" BackColor="#CCD5AE" Font-Bold="True" ForeColor="#005F73"></asp:Label>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2" colspan="2">
+                        <asp:Label ID="Label6" runat="server" Font-Bold="True" ForeColor="#E63946"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2" colspan="2">
+            <asp:Button ID="Button1" runat="server" Text="Insert" BackColor="#A1CCA5" OnClick="Button1_Click" />
+&nbsp; <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+&nbsp; <asp:Button ID="Button3" runat="server" Text="Delete" BackColor="#A1CCA5" OnClick="Button3_Click" />
+&nbsp; <asp:Button ID="Button4" runat="server" Text="Clear" OnClick="Button4_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2" colspan="2">
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Height="146px" Width="243px">
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#7C6F57" />
+            <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#E3EAEB" />
+            <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F8FAFA" />
+            <SortedAscendingHeaderStyle BackColor="#246B61" />
+            <SortedDescendingCellStyle BackColor="#D4DFE1" />
+            <SortedDescendingHeaderStyle BackColor="#15524A" />
+        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ DAY7 CRUD/CRUD.aspx.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using YASHTRY.Models;
+
+namespace YASHTRY.DAY7_CRUD
+{
+    public partial class CRUD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+                LoadAll();
+                UpdateFun();
+
+
+            }
+
+        }
+
+        private void UpdateFun()
+        {
+            CrudTryTable Ctt = new CrudTryTable();
+
+            SqlConnection cn = null;
+            SqlCommand cmd = null;
+            int Counter = 0;
+
+            try
+            {
+                cn = new SqlConnection();
+                cn.ConnectionString = @"data source=DESKTOP-1R8GHJS\SQLEXPRESS; initial catalog =tsegrp_2_2022;integrated security=true";
+                int counter = 0;
+
+                cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "USP_UPDATECTT";
+
+                cmd.Parameters.AddWithValue("id", TextBox4.Text);
+
+
+
+                cmd.Parameters.AddWithValue("@Name", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@Age", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@City", TextBox3.Text);
+
+
+
+                Counter = cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Label6.Text = ex.ToString();
+            }
+            finally
+            {
+                if (cn != null)
+                {
+                    cn.Close();
+                    if (Counter.Equals(1))
+                    {
+
+                      Label6.Text = "Updated Successfully";
+                      LoadAll();
+                    }
+                    cn.Dispose();
+                    cn = null;
+                }
+            }
+
+
+            
+
+
+        }
+
+        private void GetFun()
+        {
+
+
+            SqlConnection cn = null;
+            SqlCommand cmd = null;
+            int Counter = 0;
+
+            try
+            {
+                cn = new SqlConnection();
+                cn.ConnectionString = @"data source=DESKTOP-1R8GHJS\SQLEXPRESS; initial catalog =tsegrp_2_2022;integrated security=true";
+                cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "USP_GETCTT";
+                cn.Open();
+
+                cmd.Parameters.AddWithValue("id", TextBox4.Text);
+
+
+
+
+
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    TextBox1.Text = sdr["Name"].ToString();
+                    TextBox2.Text = sdr["Age"].ToString();
+                    TextBox3.Text = sdr["City"].ToString();
+
+                }
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Label6.Text = ex.ToString();
+            }
+            finally
+            {
+                if (cn != null)
+                {
+                    cn.Close();
+                    cn.Dispose();
+                    cn = null;
+                }
+            }
+
+
+        }
+        public void DeleteFun()
+        {
+            SqlConnection cn = null;
+            SqlCommand cmd = null;
+
+
+            try
+            {
+                cn = new SqlConnection();
+                cn.ConnectionString = @"data source=DESKTOP-1R8GHJS\SQLEXPRESS; initial catalog =tsegrp_2_2022;integrated security=true";
+                cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "USP_DELETECTT";
+                cmd.Parameters.AddWithValue("id", TextBox4.Text);
+                cmd.ExecuteNonQuery();
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (cn != null)
+                {
+                    cn.Close();
+
+                    Label6.Text = "Record Deleted Successfully";
+                    LoadAll();
+                    cn.Dispose();
+                    cn = null;
+                }
+            }
+        }
+
+        private void Clear()
+
+        {
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox1.Text = "";
+            Label6.Text = "";
+        }
+
+        private void LoadAll()
+        {
+            CrudTryTable ctt = new CrudTryTable();
+            DataSet dset = ctt.GetlistofAllData();
+            GridView1.DataSource = dset;
+            GridView1.DataBind();
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CrudTryTable Ctt = new CrudTryTable();
+
+
+            try
+            {
+                Ctt.Name = TextBox1.Text;
+                Ctt.Age = TextBox2.Text;
+                Ctt.City = TextBox3.Text;
+
+                int Counter = Ctt.InsertFun(Ctt);
+
+                if (Counter.Equals(1))
+                {
+                    Label6.Text = " !! Registered Successfully !! ";
+                    LoadAll();
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Label6.Text = ex.ToString();
+            }
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            UpdateFun();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            DeleteFun();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+            Clear();
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            GetFun();
+
+        }
+
+    } 
+}
+
+
+
+
